6a8ed469badbfb3b069aee52ce5a891b9788c1f6
Antonio Carpentieri <a.carpe@gmail.com>
2016-04-27 11:09:18 +0200

    Update AcceptanceTest.java

--- src/test/java/it/xpug/kata/birthday_greetings/AcceptanceTest.java
+++ src/test/java/it/xpug/kata/birthday_greetings/AcceptanceTest.java
@@ -2,39 +2,41 @@ package it.xpug.kata.birthday_greetings;
 
 import static org.junit.Assert.*;
 
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import org.junit.*;
 
 import com.dumbster.smtp.*;
 
+import javax.mail.Address;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 
 public class AcceptanceTest {
 
-	private static final int NONSTANDARD_PORT = 9999;
+	private static final int NONSTANDARD_PORT = 3025;
 	private BirthdayService birthdayService;
-	private SimpleSmtpServer mailServer;
+	@Rule
+	public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP);
 
 	@Before
 	public void setUp() throws Exception {
-		mailServer = SimpleSmtpServer.start(NONSTANDARD_PORT);
 		birthdayService = new BirthdayService();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		mailServer.stop();
-		Thread.sleep(200);
-	}
 
 	@Test
 	public void willSendGreetings_whenItsSomebodysBirthday() throws Exception {
 
 		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/10/08"), "localhost", NONSTANDARD_PORT);
 
-		assertEquals("message not sent?", 1, mailServer.getReceivedEmailSize());
-		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmail().next();
-		assertEquals("Happy Birthday, dear John!", message.getBody());
-		assertEquals("Happy Birthday!", message.getHeaderValue("Subject"));
-		String[] recipients = message.getHeaderValues("To");
+		MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+		assertEquals("message not sent?", 1, receivedMessages.length);
+		MimeMessage message = receivedMessages[0];
+		assertEquals("Happy Birthday, dear John!", GreenMailUtil.getBody(message));
+		assertEquals("Happy Birthday!", message.getSubject());
+		Address[] recipients = message.getRecipients(MimeMessage.RecipientType.TO);
 		assertEquals(1, recipients.length);
 		assertEquals("john.doe@foobar.com", recipients[0].toString());
 	}
@@ -43,6 +45,6 @@ public class AcceptanceTest {
 	public void willNotSendEmailsWhenNobodysBirthday() throws Exception {
 		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/01/01"), "localhost", NONSTANDARD_PORT);
 
-		assertEquals("what? messages?", 0, mailServer.getReceivedEmailSize());
+		assertEquals("what? messages?", 0, greenMail.getReceivedMessages().length);
 	}
 }
